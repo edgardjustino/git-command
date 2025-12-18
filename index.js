39c813df35883f5d5bf230096d96871b501d7e0b
edgardjustino <edgardpjustino@gmail.com>
2025-12-17 23:26:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express') //realocar ele no código 
+const port = ProcessingInstruction.env.PORT || 3001 //porta 3001 > porta de comunicaçao frequentemente usada como a porta padrao para executar aplicativos criados com a biblioteca de JS React, que é usada para criar interfaces de usuários em aplicativos da web 
+const app = express()
+
+app.listen (port, () => {
+  console.log ('start listening ${port}')
+} )
