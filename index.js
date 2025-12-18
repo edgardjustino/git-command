bc2fc009b37e8676a139f47205038662525ff220
edgardjustino <edgardpjustino@gmail.com>
2025-12-17 23:46:50 -0300

    new feature

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express') //realocar ele no código
 const port = ProcessingInstruction.env.PORT || 3001 //porta 3001 > porta de comunicaçao frequentemente usada como a porta padrao para executar aplicativos criados com a biblioteca de JS React, que é usada para criar interfaces de usuários em aplicativos da web 
 const app = express()
 
+app.get('/', (req, res) => {
+  console.log('New feature')
+}) //criando mais um endpoit
+
 app.listen (port, () => {
   console.log ('start listening ${port}')
 } )
